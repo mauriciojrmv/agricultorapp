d9a2c7e48be421f43d389bc1833daa0cbaf3742f
Mauricio Mattinen <mauricio.jr.mattinen.valdez@gmail.com>
2024-10-31 10:03:10 -0400

    seeders agregados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,20 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            AgricultorSeeder::class,
+            TerrenoSeeder::class,
+            TemporadaSeeder::class,
+            ClienteSeeder::class,
+            CategoriaSeeder::class,
+            ProductoSeeder::class,
+            // Agrega otros seeders aquí si es necesario
+        ]);
     }
 }
